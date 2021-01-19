acaa755136375fdb66760e9e0077d66dd21db166
Julie Desvaux <desvaux.julie@gmail.com>
2021-01-19 21:07:31 +0100

    [Navbar]: refactor navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,96 +11,87 @@ import './Navbar.scss'
 
 export default function NavBar() {
 
-    const { searchText, menu, smallScreen } = useSelector(state => ({ ...state.searchTextReducer }))
-    const [anchorEl, setAnchorEl] = useState(null);
+    const { searchText, smallScreen } = useSelector(state => ({ ...state.searchTextReducer }))
+    const [widthSreen, setWidthScreen] = useState(window.innerWidth)
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [menu, showMenu] = useState(false);
     const dispatch = useDispatch()
 
     useEffect(() => {
-        const mediaQuery = window.matchMedia("(max-width: 900px");
-        mediaQuery.addListener(handleMediaQueryChange);
-        handleMediaQueryChange(mediaQuery);
+        const changeWidth = () => {
+            setWidthScreen(window.innerWidth)
+            if (window.innerWidth > 900) {
+                setToggleMenu(false)
+            }
+        }
+        window.addEventListener('resize', changeWidth)
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
     }, [])
 
-    const handleMediaQueryChange = mediaQuery => {
-        if(mediaQuery.matches) {
-            dispatch({
-                type: 'SMALL_SCREEN',
-                payload: true
-            })
-        } else {
-            dispatch({
-                type: 'SMALL_SCREEN',
-                payload: false
-            })
+    function windowOnClick(event) {
+        if (!event.target.matches('.koya-icon-menu')) {
+          showMenu(false);
+          window.removeEventListener('click', windowOnClick);
+        }
+    }
+    
+    function iconOnClick(event) {
+        event.stopPropagation();
+        if (widthSreen > 900 || toggleMenu) {
+            showMenu(!menu);
         }
     }
 
-    const hideMenu = () => {
-        dispatch({
-            type: 'SHOW_MENU',
-            payload: !menu
-        })
-    }
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
     const handleSubmit = (e) => {
+        toogleNavSmallScreen();
         e.preventDefault();
     }
 
-    const toggleNavRes = () => {
-        dispatch({
-            type: 'SHOW_MENU',
-            payload: !menu
-        })
+    const toogleNavSmallScreen = () => {
+        setToggleMenu(!toggleMenu)
     }
 
     return (
         <div className="navbar-container">
             <nav className="navbar-subcontainer">
-                {(menu || !smallScreen) && (
+				<a href="#" className="link link-logo">
+					Moovice
+				</a>
+                {(widthSreen > 900 || toggleMenu) && (
                     <>
                     <ul className="listMenu">
-                        <li onClick={hideMenu} className="linksNav">
-                            <Button>
-                                <Link className="link" to="/">
-                                    Moovice
-                                </Link>
-                            </Button>
-                        </li>
-                        <li onClick={hideMenu} className="linksNav">
-                            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                                <Link className="link">
-                                    Movies
-                                </Link>
-                            </Button>
-                            <Menu
-                                id="simple-menu"
-                                anchorEl={anchorEl}
-                                keepMounted
-                                open={Boolean(anchorEl)}
-                                onClose={handleClose}
-                            >
-                                <MenuItem onClick={handleClose}>
-                                    <Link className="sublink" to="/">This Week</Link>
-                                </MenuItem>
-                                <MenuItem onClick={handleClose}>
-                                    <Link className="sublink" to="/popular">Popular</Link>
-                                </MenuItem>
-                            </Menu>
-                        </li>
-                        <li onClick={hideMenu} className="linksNav">
-                            <Button>
-                                <Link className="link" to="/my-list">
-                                    My List
-                                </Link>
-                            </Button>
+                        <div className="item-menu" onClick={(e) => iconOnClick(e)} onClick={toogleNavSmallScreen}>
+                            <li>MOOVIES</li>
+                        </div>
+                        {(widthSreen < 900 && toggleMenu) ? (
+                            <ul className="sub-menu-mobile">
+                                <a href="/">
+                                    <li className="li-menu-mobile">THIS WEEK</li>
+                                </a>
+                                <a href="/popular">
+                                    <li className="li-menu-mobile">POPULAR</li>
+                                </a>
+                            </ul>
+                        ) : (
+                            menu ? (
+                                <div className="dropdown-menu">
+                                    <ul>
+                                        <a href="/">
+                                            <li className="li-dropdown" onClick={toogleNavSmallScreen}>This Week</li>
+                                        </a>
+                                        <a href="/popular" onClick={toogleNavSmallScreen}>
+                                            <li className="li-dropdown">Popular</li>
+                                        </a>
+                                    </ul>
+                                </div>
+                            ) : null
+                        )}
+                        <li onClick={toogleNavSmallScreen} className="linksNav">
+							<a className="link" href="/my-list" onClick={toogleNavSmallScreen}>
+								MY LIST
+							</a>
                         </li>
                     </ul>
                     <ul className="listMenu search-bar">
@@ -109,7 +100,7 @@ export default function NavBar() {
                                 <input value={searchText} onChange={(e) => dispatch({type: 'ADD_SEARCH_TEXT', payload: e.target.value})} type="text" className="inputSearch" onFocus={(e) => dispatch({type: 'ADD_SEARCH_TEXT', payload: ""})}/>
                                 <Link className="link" to={{pathname: `/search/${searchText}`}}>
                                     <button type="submit" className="btn-search">
-                                        <img src={search} alt="icon magnifying glass" className="logoGlass"/>
+                                        <img src={search} alt="icon magnifying glass" className="logoGlass" onClick={toogleNavSmallScreen}/>
                                     </button>
                                 </Link>
                             </form>
@@ -118,10 +109,78 @@ export default function NavBar() {
                     </>
                 )}
             </nav>
-            <div className="menuResBtn">
-                <img onClick={toggleNavRes} src={!menu ? menuIco : cross} alt="Menu Icon Responsive" className="menuIco"/>
+            <div className={(widthSreen > 900 || toggleMenu) ? "menuResBtn active" : "menuResBtn"} >
+				<div className="line" onClick={toogleNavSmallScreen}></div>
             </div>
         </div>
+
+
+
+
+
+
+
+
+        // <div className="navbar-container">
+        //     <nav className="navbar-subcontainer">
+        //         {(widthSreen > 900 || toggleMenu) && (
+        //             <>
+        //             <ul className="listMenu">
+        //                 <li onClick={hideMenu} className="linksNav">
+        //                     <Button>
+        //                         <Link className="link" to="/">
+        //                             Moovice
+        //                         </Link>
+        //                     </Button>
+        //                 </li>
+        //                 <li onClick={hideMenu} className="linksNav">
+        //                     <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        //                         <Link className="link">
+        //                             Movies
+        //                         </Link>
+        //                     </Button>
+        //                     <Menu
+        //                         id="simple-menu"
+        //                         anchorEl={anchorEl}
+        //                         keepMounted
+        //                         open={Boolean(anchorEl)}
+        //                         onClose={handleClose}
+        //                     >
+        //                         <MenuItem onClick={handleClose}>
+        //                             <Link className="sublink" to="/">This Week</Link>
+        //                         </MenuItem>
+        //                         <MenuItem onClick={handleClose}>
+        //                             <Link className="sublink" to="/popular">Popular</Link>
+        //                         </MenuItem>
+        //                     </Menu>
+        //                 </li>
+        //                 <li onClick={hideMenu} className="linksNav">
+        //                     <Button>
+        //                         <Link className="link" to="/my-list">
+        //                             My List
+        //                         </Link>
+        //                     </Button>
+        //                 </li>
+        //             </ul>
+        //             <ul className="listMenu search-bar">
+        //                 <li className="linksNav">
+        //                     <form action="" className="formSubmit" onSubmit={handleSubmit}>
+        //                         <input value={searchText} onChange={(e) => dispatch({type: 'ADD_SEARCH_TEXT', payload: e.target.value})} type="text" className="inputSearch" onFocus={(e) => dispatch({type: 'ADD_SEARCH_TEXT', payload: ""})}/>
+        //                         <Link className="link" to={{pathname: `/search/${searchText}`}}>
+        //                             <button type="submit" className="btn-search">
+        //                                 <img src={search} alt="icon magnifying glass" className="logoGlass"/>
+        //                             </button>
+        //                         </Link>
+        //                     </form>
+        //                 </li>
+        //             </ul>
+        //             </>
+        //         )}
+        //     </nav>
+        //     <div className="menuResBtn">
+        //         <img onClick={toggleNavRes} src={!menu ? menuIco : cross} alt="Menu Icon Responsive" className="menuIco"/>
+        //     </div>
+        // </div>
     )
 
 }
